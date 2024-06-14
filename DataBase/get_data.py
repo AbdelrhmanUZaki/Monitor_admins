90701bdbf95990624eb285a3502705840388bdcc
zaki <AbdelrhmanUZaki@gmail.com>
2024-06-14 14:44:13 +0300

    Separate database.py into 3 files (get_data.py, schema.py, set_data.py)

---
+++ DataBase/get_data.py
@@ -0,0 +1,28 @@
+from sqlite3 import connect
+
+conn = connect('database.db')
+cur = conn.cursor()
+
+def get_users_info():
+    cur.execute("SELECT * FROM combination")
+    for item in cur.fetchall():
+        print(item)
+
+
+def get_source_info():
+    cur.execute('''
+    SELECT admin_id, source_group_id FROM combination
+    ''')
+    all_data = cur.fetchall()
+    print(f"type of get_source_info\n{type(all_data)}")
+    print(f"data of get_source_info\n{all_data}")
+
+
+def get_destination_info(destination_group_id, destination_thread_id):
+    cur.execute('''SELECT destination_group_id, destination_thread_id FROM combination
+                WHERE admin_id = ? AND source_group_id = ?''',
+                (destination_group_id, destination_thread_id))
+    all_data = cur.fetchall()
+    print(type(all_data))
+    print(f"type of get_destination_info\n {type(all_data)}")
+    print(f"data of get_destination_info\n {all_data}")
---
+++ DataBase/set_data.py
@@ -0,0 +1,13 @@
+from sqlite3 import connect
+
+conn = connect('database.db')
+cur = conn.cursor()
+
+
+def insert_in_combination_table(values):
+    cur.execute('''INSERT INTO combination
+                ( bot_user_id, admin_name, 
+                 admin_id, source_group_id, source_group_name,
+                 destination_group_id, destination_group_name,
+                 destination_thread_id, date)
+                VALUES (?,?,?,?,?,?,?,?,?)''', values)
