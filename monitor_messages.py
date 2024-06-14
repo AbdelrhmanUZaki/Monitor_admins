d615a917133f517f6d9109d873a33f9231bfb119
zaki <AbdelrhmanUZaki@gmail.com>
2024-06-14 23:31:31 +0300

    monitor_messages.py: > create fetch_all_combinations function to retrieve combinations for current user

--- monitor_messages.py
+++ monitor_messages.py
@@ -1,7 +1,16 @@
 from database_scripts.schema import cur
 
 
-def fetch_all_combinations():
-    cur.execute("SELECT * FROM combination")
+def fetch_all_combinations(bot_user_id, bot_user_name):
+    cur.execute("SELECT DISTINCT source_group_name, destination_group_name FROM combination WHERE bot_user_id = ?", (bot_user_id,))
     data = cur.fetchall()
-    return data
+    msg = f"مرحبا {bot_user_name}\n\n"
+    msg += "المجموعات التي يتم متابعتها\n"
+    for item in data:
+        # item 0 -> Source Group Name
+        # item 1 -> Destination Group Name
+        msg += f"- {item[0]} => {item[1]}\n"
+    return msg
+
+def fetach_admin_ids(bot_user_id):
+    pass
