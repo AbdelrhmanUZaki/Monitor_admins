34788e7b84c52c32d1e9486c52a22ac78d16d5a3
zaki <AbdelrhmanUZaki@gmail.com>
2024-06-14 17:21:49 +0300

    Create handler to retrieve combinations list

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message
 from pyrogram import filters
 from helpers import on_join, add_new_user
 from database_scripts.schema import create_db_tables
+from monitor_messages import fetch_all_combinations
 
 
 @app.on_message(filters.command(["start"]))
@@ -18,6 +19,13 @@ async def handle_message(client, message: Message):
     msg += "\n\nNote: You shouldn't be hidden in that group at least when using this bot"
     await message.reply(msg, reply_to_message_id=message.id)
 
+@app.on_message(filters.command(['combinations']))
+async def get_combinations(client, message: Message):
+    combinations = fetch_all_combinations()
+    msg = f"{combinations}"
+    await message.reply(msg, reply_to_message_id=message.id)
+
+    
 
 if __name__ == "__main__":
     create_db_tables()
